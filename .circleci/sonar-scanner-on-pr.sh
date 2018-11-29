969d0a49c03bf87dc42063fa1a7a8facd403c1ce
YOU54F <yousafn@gmail.com>
2018-11-29 00:55:38 +0000

    path correct

--- .circleci/sonar-scanner-on-pr.sh
+++ .circleci/sonar-scanner-on-pr.sh
@@ -1,6 +1,6 @@
 if [[ ((`echo $CIRCLE_PULL_REQUEST | grep -c "pull"` > 0))]]; then 
       GIT_PR_NUMBER=$(echo ${CIRCLE_PULL_REQUEST} | sed 's#.*/##' ) &&          
-      ~/app/node_modules/.bin/sonar-scanner \
+      ~/app/cypress/node_modules/.bin/sonar-scanner \
       -Dsonar.host.url=https://sonarcloud.io \
       -Dsonar.projectKey=$SONAR_PROJECTKEY \
       -Dsonar.organization=$SONAR_ORGANIZATION \
